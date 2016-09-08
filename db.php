4b71743ea7699e30e18b91cde9e7ef5033c1c147
Murad Ramazanli <qargali@outlook.com>
2016-09-08 10:01:39 +0400

    We dont need db.php anymore

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    try{
-        $db = new PDO('mysql:host=mysql.hostinger.ru;dbname=u782632263_baza','u782632263_baza','9gf3az2q');
-    }
-    catch(PDOException $e){
-        echo $e->getMessage();
-    }
-?>
